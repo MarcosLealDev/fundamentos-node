ef46637ee5f6250b4ed3b61cd03c3714d736c14e
Marcos Leal <marcosleal@gmail.com>
2020-08-25 18:20:14 -0400

    fix: making sure type is income or outcome only

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -21,7 +21,12 @@ class CreateTransactionService {
   }
 
   public execute({ title, value, type }: Request): Transaction {
+    if (!['income', 'outcome'].includes(type)) {
+      throw new Error('Transaction type is invalid');
+    }
+
     const { total } = this.transactionsRepository.getBalance();
+
     if (type === 'outcome' && total < value) {
       throw Error('There is no sufficient balance.');
     }
